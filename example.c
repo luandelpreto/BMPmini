402e16b5943444e582fe7eefadfd579536c1f18d
Luan <delpretob13@hotmail.com>
2021-01-19 15:35:55 -0400

    Change packed struct approach to raw byte parsing

--- example.c
+++ example.c
@@ -4,27 +4,31 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        fprintf(stderr, "[USAGE]:\n%s <BMP image path>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
     BMPmini_image *img = BMPmini_read(argv[1]);
 
     if (img) {
         int res = BMPmini_write("images/results/testeBMP.bmp", img);
         if (res != BMPmini_SUCCESS) {
-            fprintf(stderr, "FAIL TO WRITE\n");
+            fprintf(stderr, "FAILED TO WRITE '%s'\n", argv[1]);
         }
     }
     else {
-        fprintf(stderr, "NULL IMG\n");
+      fprintf(stderr, "FAILED TO READ '%s'\n", argv[1]);
     }
 
-    BMPmini_image *newimg = BMPmini_crop(img, 2500, 2000, 2500, 1000);
+    BMPmini_image *newimg = BMPmini_crop(img, 0, 0, 500, 500);
     if (newimg) {
         int res = BMPmini_write("images/results/testeCroppedBMP.bmp", newimg);
         if (res != BMPmini_SUCCESS) {
-            fprintf(stderr, "FAIL TO WRITE 2\n");
+            fprintf(stderr, "FAILED TO WRITE '%s'\n", argv[1]);
         }
     }
     else {
-        fprintf(stderr, "NULL IMG 2\n");
+        fprintf(stderr, "FAILED TO READ '%s'\n", argv[1]);
     }
 
     BMPmini_free(img);
